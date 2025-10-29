e29181c9ff0c9112c54ab5f8f47d32b6511acbfe
abh3shek <abhishekganapure22@gmail.com>
2025-10-29 18:58:49 +0530

    :/

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ from joblib import load
 nltk_data_dir = os.path.join(os.path.dirname(__file__), "nltk_data")
 os.makedirs(nltk_data_dir, exist_ok=True)
 
-# Append the folder to NLTK's search path **before using any NLTK functions**
+# Add to NLTK path
 nltk.data.path.append(nltk_data_dir)
 
-# Download required NLTK data if not already present
+# Download required packages if not already downloaded
 try:
-    nltk.data.find('tokenizers/punkt')
+    nltk.data.find("tokenizers/punkt")
 except LookupError:
-    nltk.download('punkt', download_dir=nltk_data_dir)
+    nltk.download("punkt", download_dir=nltk_data_dir)
 
 try:
-    nltk.data.find('corpora/stopwords')
+    nltk.data.find("corpora/stopwords")
 except LookupError:
-    nltk.download('stopwords', download_dir=nltk_data_dir)
+    nltk.download("stopwords", download_dir=nltk_data_dir)
 
 stop_words = set(stopwords.words('english'))
 stemmer = PorterStemmer()
